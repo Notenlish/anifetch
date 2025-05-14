56ffabca1b9c9461b0d1181a089b0d70a0bb18ed
Immelancholy <lenalowes0@gmail.com>
2025-05-14 12:12:50 +0100

    remove redundant variable

--- anifetch.py
+++ anifetch.py
@@ -258,7 +258,6 @@ def chafa_files(code):
         f = str(i) + ".png"
         path = BASE_PATH / "video" / f
         if os.path.exists(path):
-            last_f = f
             thread_chafa = threading.Thread(target=chafa_process, args=(f, ))
             thread_chafa.start()
             threads.append(thread_chafa)
