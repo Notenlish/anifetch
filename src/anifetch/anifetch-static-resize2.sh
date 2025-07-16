239a5659ec6de66826dda7be95b0cab43a0edfe4
jdafoe12 <jwdafoe1@gmail.com>
2025-07-16 02:33:00 -0400

    changed cursor position

--- src/anifetch/anifetch-static-resize2.sh
+++ src/anifetch/anifetch-static-resize2.sh
@@ -47,7 +47,7 @@ cleanup() {
   fi
   tput sgr0          # Reset terminal attributes
   
-  cursor_pos=$((bottom + 5))
+  cursor_pos=$((bottom + 10))
   tput cup $cursor_pos 0
   
   # Echo the captured key in background after a delay (only if key-exit is enabled)
