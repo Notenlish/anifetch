206678f48b03f1522315ba8dc0c604fd41134849
Gallophostrix <gallophostrix@gmail.com>
2025-07-26 00:00:25 +0200

    Filename help

--- src/anifetch/__init__.py
+++ src/anifetch/__init__.py
@@ -3,11 +3,20 @@ Anifetch package initialization module.
 """
 
 from .core import run_anifetch
-from .cli import parse_args
-
+from .cli import parse_args, parser
+import sys
 
 def main():
     args = parse_args()
+
+    allowed_alternatives = ["cache_list", "clear", "delete"]
+
+    if (
+        args.filename is None
+        and not any(getattr(args, key) for key in allowed_alternatives)
+    ):
+        print("[ERROR] Missing input. Use a filename or an option like --cache-list.")
+        sys.exit(1)
 
     run_anifetch(args)
 
